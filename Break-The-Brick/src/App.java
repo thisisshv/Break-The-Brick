375c8a396a7faf7e3f508ad6728055dedcb88466
Shivanshu <shv.shivanshu@gmail.com>
2022-12-15 02:00:07 +0530

    first commit

---
+++ Break-The-Brick/src/App.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+
+public class App {
+    public static void main(String[] args) {
+        JFrame obj = new JFrame();
+        obj.setBounds(10, 10, 700, 600);
+        obj.setTitle("Break The Brick");
+
+    }
+}
